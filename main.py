a677ecfd32cf903272e7d65de4c10dd3ba36b9a9
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2020-10-26 18:10:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0', debug=True)
